a8b1e5bf26d313c8c37ed706d82e60faec913fbe
tomo6300 <tomoyagishita@icloud.com>
2021-10-26 01:11:51 +0900

    add Django & react

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import ReactDom from "react-dom"
 
-import App from "./components/App.js";
+import App from "../src/components/App.js";
 
 let myComponent = document.getElementById("app")
-if (myComponent !== null){
-    ReactDom.render(<App />, myComponent)
+if (myComponent !== null) {
+    ReactDom.render( < App / > , myComponent)
 }
